932053beb0d90297bbadae56cad173969b0bf770
Gabriel <gabriel.dev09@gmail.com>
2024-10-12 09:34:21 -0300

    Alterações vps

--- VozDoMorro/VozDoMorro/settings.py
+++ VozDoMorro/VozDoMorro/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'voz_do_morro_sql',
         'USER': 'ongvozdomorro',
-        'PASSWORD' : '@ongvozdomorro102030',
+        'PASSWORD' : 'root',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
